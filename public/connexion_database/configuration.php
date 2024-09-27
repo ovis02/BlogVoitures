2967de8b92091eb1f014b4a26b5e684305777cb0
MOHAMMAD Aowis <oves7860@gmail.com>
2024-09-27 14:33:26 +0200

    database heroku

--- public/connexion_database/configuration.php
+++ public/connexion_database/configuration.php
@@ -2,7 +2,7 @@
 $host = 'q2gen47hi68k1yrb.chr7pe7iynqr.eu-west-1.rds.amazonaws.com'; // Host distant
 $db = 'ofdf6vm86czeemf4'; // Nom de la base de données
 $user = 'cfwgxbjq7k4lk44u'; // Nom d'utilisateur
-$pass = 'lhw1l9pcmt5yvos0'; // Mot de passe
+$pass = 'z4ov5t4efw1ej0k2'; // Mot de passe
 
 $dsn = "mysql:host=$host;dbname=$db;charset=utf8mb4";
 
