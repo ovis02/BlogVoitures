b95e74501fb9c89d6ef5ba55beb1bf72c3d59658
MOHAMMAD Aowis <oves7860@gmail.com>
2024-09-27 21:56:30 +0200

    id change bug js nav

--- public/index.php
+++ public/index.php
@@ -41,7 +41,7 @@ $validated_comments = $stmt->fetchAll(PDO::FETCH_ASSOC);
     <header>
      
              <nav>
-              <img class="fond-ecran" id="car-light" src="images/lightOff.png" />
+              <img class="fond-ecran" id="car-light" src="images/lightoff.png" />
 
         <div class="plate-number">
           <div class="rectangle">
